9525d9914f2882585923e5bdb53f9eb6bbfc136d
sidoine2211-p <sidoineamoussouy@gmaile.com>
2025-01-29 05:36:23 +0100

    Protocole energie principale

--- exo_2/protocole1.dart
+++ exo_2/protocole1.dart
@@ -2,4 +2,41 @@
 
 
 
-hhn,snxjkxnkjxnnj
+int distance = 80;
+
+choixDistance(distance);
+
+pointsRecharge(distance);
+
+
+
+}
+
+
+int choixDistance(int distance){
+
+if(distance%2==0) {
+
+print("Activation mode rapide");
+
+} else {
+
+print("Activation mode éco+");
+
+}
+
+return distance;
+
+}
+
+
+int pointsRecharge(int distance) {
+
+double recharge1 = distance/10;
+int arrondirRecharge= recharge1.ceil();
+
+print("Le nombre de recharge pour cette distance est : $arrondirRecharge recharges dont une après chaque 10km");
+
+return arrondirRecharge;
+
+}
